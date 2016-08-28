d096c4bfa98ff779f040b163fda0cf6d12bfaabd
Chris Sells <csells@sellsbrothers.com>
2016-08-27 20:50:37 -0700

    initial commit

---
+++ Lesson_2/000_Hello_APIs/src/ConferenceCentral/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceCentral.Controllers {
+  [Route("api/[controller]")]
+  public class HelloController : Controller {
+    const string message = "Hello World";
+
+    // GET api/hello
+    [HttpGet]
+    public string Get() {
+      return message;
+    }
+
+    // GET api/hello/bob
+    [HttpGet("{name}")]
+    public string Get(string name) {
+      return "Hello " + name + "!";
+    }
+  }
+}
---
+++ Lesson_2/000_Hello_APIs/src/ConferenceCentral/wwwroot/js/home.js
@@ -0,0 +1,26 @@
+﻿/*
+ * Execute a request to the hello API
+ */
+function greetGenerically() {
+  // Call the hello api.
+  $.ajax("/api/hello/").done(helloCallback);
+}
+
+/*
+ * Execute a request to the hello API.
+ * Illustrates calling an API that takes an argument.
+ */
+function greetByName() {
+  // Get the name from the name_field element
+  var name = document.getElementById("name_field").value;
+
+  // Call the hello api, passing in the name
+  $.ajax("/api/hello/" + name).done(helloCallback);
+}
+
+// Process the JSON response
+// In this case, just show an alert dialog box
+// displaying the the response message
+function helloCallback(message) {
+  alert(message);
+}
